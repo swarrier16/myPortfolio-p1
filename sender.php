c20097b864b9722232b249d8fd9964bc11a51362
swarrier16 <swarrier16@gmail.com>
2018-02-18 18:43:00 -0500

    Finalize site details for deployment

--- sender.php
+++ sender.php
@@ -7,8 +7,8 @@
  **************************************
  */
 
-$to = 'YOUR_EMAIL_ADDRESS';
-$siteName = "YOUR_SITE_NAME";
+$to = 'ssw7@njit.edu';
+$siteName = "portfolio";
 
 /*
  *************************************************************
